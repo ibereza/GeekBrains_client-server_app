bd7586935a7598c74e1cf60eb1f85f108232662d
Igor Bereza <igor.bereza@gmail.com>
2022-02-01 13:39:40 +0200

    added function logging

--- logs/logs_config/function_log.py
+++ logs/logs_config/function_log.py
@@ -5,14 +5,14 @@ import sys
 from . import client_log_config
 from . import server_log_config
 
-if sys.argv[0] == 'client.py':
-    client_log = logging.getLogger('client')
+if sys.argv[0].endswith('client.py'):
+    function_log = logging.getLogger('client')
 else:
-    client_log = logging.getLogger('server')
+    function_log = logging.getLogger('server')
 
 
 def log(func):
     def func_to_log(*args, **kwargs):
-        client_log.info(f'Function "{func.__name__}" was called from function "{inspect.stack()[1][3]}"')
+        function_log.info(f'Function "{func.__name__}" was called from function "{inspect.stack()[1][3]}"')
         return func(*args, **kwargs)
     return func_to_log
